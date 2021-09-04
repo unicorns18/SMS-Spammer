f2be312039c41127bf5166cbf3773841ebaa49c8
unicorns18 <77941625+unicorns18@users.noreply.github.com>
2021-09-05 03:48:38 +1000

    Update message.py
    Added load_dotenv to make life easier adding your API keys :)

--- message.py
+++ message.py
@@ -1,5 +1,8 @@
 import vonage
+from dotenv import load_dotenv
 from colorama import Fore, Back, Style
+
+load_dotenv()
 
 print(Fore.RED + "!!! PUT THE NUMBER IN A E.164 FORMAT !!! " + Fore.GREEN + "(Example: +19998887777)")
 print(Fore.RED + "!!! PUT THE NUMBER IN A E.164 FORMAT !!! " + Fore.GREEN + "(Example: +19998887777)")
@@ -12,8 +15,11 @@ print(Style.RESET_ALL)
 phone_number = int(input("What phone number do you want to message: "))
 message_body = str(input("What is the message you want to send: "))
 
+api_key = os.environ.get("key")
+api_secret = os.environ.get("secret")
+
 # Add your key and secret key from Vonage here! :)
-client = vonage.Client(key='YOUR_KEY', secret='YOUR_SECRET_KEY')
+client = vonage.Client(key=api_key, secret=api_secret)
 sms = vonage.Sms(client)
 
 # Put search_pattern to whatever you want, don't touch pattern. (1 = if that search pattern is in any of the number)
